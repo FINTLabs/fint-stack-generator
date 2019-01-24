ce01f3b3a8adaae32fb36000f94a018966900c09
Asgeir Nilsen <asgeir@fintlabs.no>
2019-01-24 11:25:18 +0100

    Added support for generating Kubernetes stack.

---
+++ src/main/groovy/no/fint/stack/generator/KubeGenerator.groovy
@@ -0,0 +1,78 @@
+package no.fint.stack.generator
+
+
+import io.kubernetes.client.models.V1Deployment
+import io.kubernetes.client.models.V1EnvVar
+import io.kubernetes.client.util.Yaml
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
+import org.springframework.stereotype.Service
+
+@Service
+@ConditionalOnProperty(name = "fint.stack.type", havingValue = "Kubernetes")
+class KubeGenerator implements Generator {
+
+    @Override
+    String generate(StackModel model) throws Exception {
+        def resource = getClass().getResourceAsStream('/k8s-stack.yaml')
+        def stack = Yaml.loadAll(new InputStreamReader(resource))
+
+        def provider = getDeployment('provider', stack)
+        def consumer = getDeployment('consumer', stack)
+
+        if (model.stack) {
+            stack.each {
+                it.metadata.labels['fint.stack'] = model.stack
+                it.metadata.name = "${it.metadata.labels['fint.role']}-${model.stack}"
+            }
+            stack.findAll { it.kind == 'Service'}.each { it.spec.selector['fint.stack'] = model.stack }
+            stack.findAll { it.kind == 'Deployment'}.each {
+                it.spec.selector.matchLabels['fint.stack'] = model.stack
+                it.spec.template.metadata.labels['fint.stack'] = model.stack
+                it.spec.template.spec.containers*.name = "${it.metadata.labels['fint.role']}-${model.stack}"
+                setenv(it, 'fint.hazelcast.kubernetes.labelValue', model.stack)
+            }
+        }
+
+        if (model.environment) {
+            def env = DockerGenerator.env(model.environment)
+            setenv(consumer, 'fint.audit.mongo.databasename', "fint-audit${env}")
+            setenv(provider, 'fint.audit.mongo.databasename', "fint-audit${env}")
+            setenv(consumer, 'fint.relations.default-base-url', "https://${model.environment}.felleskomponent.no")
+        }
+
+        if (model.uri) {
+            setenv(consumer, 'server.context-path', model.uri)
+            setenv(provider, 'server.context-path', model.uri + '/provider')
+
+            consumer.spec.template.spec.containers.each { it.readinessProbe.httpGet.path = model.uri + '/health' }
+            provider.spec.template.spec.containers.each { it.readinessProbe.httpGet.path = model.uri + '/provider/health' }
+        }
+
+        if (model.assets) {
+            setenv(provider, 'fint.provider.assets.endpoint', model.assets)
+        }
+
+        if (model.repository) {
+            if (model.consumer && model.version) {
+                consumer.spec.template.spec.containers.each { it.image = "${model.repository}/${model.consumer}:${model.version}" }
+            }
+            if (model.provider) {
+                provider.spec.template.spec.containers.each { it.image = "${model.repository}/provider:${model.provider}" }
+            }
+        }
+
+        return Yaml.dumpAll(stack.iterator())
+    }
+
+    static V1Deployment getDeployment(name, stack) {
+        stack.find { it.metadata.labels['fint.role'] == name && it.kind == 'Deployment' } as V1Deployment
+    }
+
+    static V1EnvVar getenv(deployment, name) {
+        deployment.spec.template.spec.containers.env.collect { it.find { it.name == name } }.first() as V1EnvVar
+    }
+
+    static setenv(deployment, name, value) {
+        getenv(deployment, name).value = value
+    }
+}
---
+++ src/test/groovy/no/fint/stack/generator/KubeGeneratorSpec.groovy
@@ -0,0 +1,99 @@
+package no.fint.stack.generator
+
+import io.kubernetes.client.JSON
+import io.kubernetes.client.util.Yaml
+import spock.lang.Specification
+
+class KubeGeneratorSpec extends Specification {
+
+    def "Parse k8s-stack.yaml and serialize as JSON"() {
+        given:
+        def res = getClass().getResourceAsStream('/k8s-stack.yaml')
+        def stack = Yaml.loadAll(new InputStreamReader(res))
+        def json = new JSON();
+
+        when:
+        def result = json.serialize(stack)
+
+        then:
+        result
+    }
+
+    def "Can parse the k8s-stack.yaml file"() {
+        given:
+        def res = getClass().getResourceAsStream('/k8s-stack.yaml')
+
+        when:
+        def result = Yaml.loadAll(new InputStreamReader(res))
+
+        then:
+        result
+        result.size() == 4
+        result.any { it.metadata.name == "consumer-administrasjon-kodeverk" }
+        result.every { it.metadata.labels['fint.stack'] == 'administrasjon-kodeverk' }
+
+        when:
+        def consumerDeployment = KubeGenerator.getDeployment('consumer', result)
+        println(consumerDeployment.spec.template.spec.containers*.resources)
+
+        then:
+        consumerDeployment
+
+        when:
+        def baseUrl = KubeGenerator.getenv(consumerDeployment, 'fint.relations.default-base-url')
+        println(baseUrl)
+
+        then:
+        baseUrl
+
+        when:
+        def providerDeployment = KubeGenerator.getDeployment('provider', result)
+
+        then:
+        providerDeployment
+
+        when:
+        def servletPath = providerDeployment.spec.template.spec.containers[0].env.find {
+            it.name == 'server.context-path'
+        }.value
+        println(servletPath)
+
+        then:
+        servletPath
+    }
+
+    def 'Produces valid output for default input'() {
+        given:
+        def generator = new KubeGenerator()
+        def model = new StackModel()
+
+        when:
+        def result = generator.generate(model)
+
+        then:
+        result
+    }
+
+    def 'Produces valid output from a defined stack'() {
+        given:
+        def stack = new StackModel(
+                environment: 'beta',
+                stack: 'utdanning-elev',
+                uri: '/utdanning/elev',
+                port: 8113,
+                repository: 'fintlabs.azurecr.io',
+                consumer: 'consumer-utdanning-elev',
+                version: '0.8.0-3.1.0',
+                provider: '2.0.1',
+                assets: new URI('https://admin.fintlabs.no/api/components/assets/utdanning_elev')
+        )
+        def generator = new KubeGenerator()
+
+        when:
+        def result = generator.generate(stack)
+        println(result)
+
+        then:
+        result
+    }
+}
